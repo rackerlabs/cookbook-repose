9b0ad1d62c2a3a579fdc748f2c6600d870f37f53
jfwood <john.wood@rackspace.com>
2013-11-12 17:59:12 -0600

    Fix per Brendan review.

--- recipes/filter-client-auth.rb
+++ recipes/filter-client-auth.rb
@@ -15,17 +15,17 @@ unless node['repose']['filters'].include? 'client-auth'
 end
 
 template '/etc/repose/client-auth-n.cfg.xml' do
-    owner node['repose']['owner']
-    group node['repose']['group']
-    mode '0644'
-    variables(
-      :auth_provider => node['repose']['client_auth']['auth_provider'],
-      :username => node['repose']['client_auth']['username_admin'],
-      :password => node['repose']['client_auth']['password_admin'],
-      :auth_uri => node['repose']['client_auth']['auth_uri'],
-      :mapping_regex => node['repose']['client_auth']['mapping_regex'],
-      :mapping_type => node['repose']['client_auth']['mapping_type'],
-      :tenant_id => node['repose']['client_auth']['tenant_id']
-    )
-    notifies :restart, 'service[repose-valve]'
+  owner node['repose']['owner']
+  group node['repose']['group']
+  mode '0644'
+  variables(
+    :auth_provider => node['repose']['client_auth']['auth_provider'],
+    :username => node['repose']['client_auth']['username_admin'],
+    :password => node['repose']['client_auth']['password_admin'],
+    :auth_uri => node['repose']['client_auth']['auth_uri'],
+    :mapping_regex => node['repose']['client_auth']['mapping_regex'],
+    :mapping_type => node['repose']['client_auth']['mapping_type'],
+    :tenant_id => node['repose']['client_auth']['tenant_id']
+  )
+  notifies :restart, 'service[repose-valve]'
 end
