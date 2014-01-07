6bd62581c14cb921bdd71f7b4bad514031c8b687
Brendan ODonnell <brendan.james.odonnell@gmail.com>
2014-01-06 19:55:09 -0600

    Loosen the version contraint on yum

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.3.0"
 
 depends          "apt"
-depends          "yum", '~> 3.0.4'
+depends          "yum", '~> 3.0'
 depends          "yum-epel"
