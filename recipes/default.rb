436d2e6ee4a5a6bd9c82c7898e88e59c02aa85a9
Brendan ODonnell <brendan.james.odonnell@gmail.com>
2014-01-09 16:12:23 -0600

    Collapse the config templates stanza

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,43 +22,19 @@ directory "#{node['repose']['config_directory']}" do
   mode '0755'
 end
 
-template "#{node['repose']['config_directory']}/container.cfg.xml" do
-  owner node['repose']['owner']
-  group node['repose']['group']
-  mode '0644'
-  variables(
-    :port => node['repose']['port'],
-    :ssl_port => node['repose']['ssl_port'],
-    :connection_timeout => node['repose']['connection_timeout'],
-    :read_timeout => node['repose']['read_timeout'],
-    :deploy_auto_clean => node['repose']['deploy_auto_clean'],
-    :filter_check_interval => node['repose']['filter_check_interval']
-  )
-  notifies :restart, 'service[repose-valve]'
-end
-
-template "#{node['repose']['config_directory']}/system-model.cfg.xml" do
-  owner node['repose']['owner']
-  group node['repose']['group']
-  mode '0644'
-  variables(
-    :cluster_id => node['repose']['cluster_id'],
-    :peers => node['repose']['peers'],
-    :filters => node['repose']['filters'],
-    :services => node['repose']['services'],
-    :endpoints => node['repose']['endpoints']
-  )
-  notifies :restart, 'service[repose-valve]'
-end
-
-template "#{node['repose']['config_directory']}/log4j.properties" do
-  owner node['repose']['owner']
-  group node['repose']['group']
-  mode '0644'
-  variables(
-    :loglevel => node['repose']['loglevel']
-  )
-  notifies :restart, 'service[repose-valve]'
+%w{ container.cfg.xml
+    system-model.cfg.xml
+    log4j.properties
+}.each do |filename|
+  template "#{node['repose']['config_directory']}/#{filename}" do
+    owner node['repose']['owner']
+    group node['repose']['group']
+    mode '0644'
+    variables(
+      node['repose']
+    )
+    notifies :restart, 'service[repose-valve]'
+  end
 end
 
 node['repose']['filters'].each do |filter|
