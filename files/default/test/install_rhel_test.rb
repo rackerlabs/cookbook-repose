2fe16f900559b4f4975e96ad3e416ae518353a58
Brendan ODonnell <brendan.james.odonnell@gmail.com>
2014-12-03 16:59:23 -0600

    Fix a bug in the test coverage for RHEL installs

--- files/default/test/install_rhel_test.rb
+++ files/default/test/install_rhel_test.rb
@@ -4,7 +4,7 @@ describe_recipe 'repose::install_rhel' do
 
   it 'installs Repose' do
     package('repose-valve').must_be_installed
-    package('repose-filters').must_be_installed
+    package('repose-filter-bundle').must_be_installed
   end
 
 end
