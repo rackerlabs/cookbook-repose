3718c108479774ab6d4f4ba1bcfa9f8dd43adb10
Nick Silkey <nick.silkey@rackspace.com>
2014-12-05 12:09:08 -0600

    Fix trailing comma issue with vars in connection pool recipe.

--- recipes/service-connection-pool.rb
+++ recipes/service-connection-pool.rb
@@ -14,12 +14,12 @@ template "#{node['repose']['config_directory']}/http-connection-pool.cfg.xml" do
     :max_total => node['repose']['connection_pool']['max_total'],
     :max_per_route => node['repose']['connection_pool']['max_per_route'],
     :socket_timeout => node['repose']['connection_pool']['socket_timeout'],
-    :connection_timeout => node['repose']['connection_pool']['connection_timeout']
-    :socket_buffer_size=> node['repose']['connection_pool']['socket_buffer_size']
-    :connection_max_line_length=> node['repose']['connection_pool']['connection_max_line_length']
-    :connection_max_header_count=> node['repose']['connection_pool']['connection_max_header_count']
-    :connection_max_status_line_garbage=> node['repose']['connection_pool']['connection_max_status_line_garbage']
-    :tcp_nodelay=> node['repose']['connection_pool']['tcp_nodelay']
+    :connection_timeout => node['repose']['connection_pool']['connection_timeout'],
+    :socket_buffer_size=> node['repose']['connection_pool']['socket_buffer_size'],
+    :connection_max_line_length=> node['repose']['connection_pool']['connection_max_line_length'],
+    :connection_max_header_count=> node['repose']['connection_pool']['connection_max_header_count'],
+    :connection_max_status_line_garbage=> node['repose']['connection_pool']['connection_max_status_line_garbage'],
+    :tcp_nodelay=> node['repose']['connection_pool']['tcp_nodelay'],
     :keepalive_timeout=> node['repose']['connection_pool']['keepalive_timeout']
   )
   notifies :restart, 'service[repose-valve]'
