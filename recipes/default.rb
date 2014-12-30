07008785e6c2d0aadcf304356079fdf284b14a35
Brendan ODonnell <brendan.james.odonnell@gmail.com>
2014-12-30 15:44:13 -0600

    Cleanup order and whitespace around filter_cluster_map

--- recipes/default.rb
+++ recipes/default.rb
@@ -32,14 +32,14 @@ service_cluster_map = {
 
 filters = node['repose']['filters']
 filter_cluster_map = {
-  'client-auth'          => node['repose']['client_auth'       ]['cluster_id'],
-  'ip-identity'          => node['repose']['ip_identity'       ]['cluster_id'],
-  'rate-limiting'        => node['repose']['rate_limiting'     ]['cluster_id'],
-  'slf4j-http-logging'   => node['repose']['slf4j_http_logging']['cluster_id'],
-  'header-translation'   => node['repose']['header_translation']['cluster_id'],
-  'derp'                 => node['repose']['derp']['cluster_id'],
-  'header-normalization' => node['repose']['header_normalization']['cluster_id'],
-  'content-type-stripper' => node['repose']['content_type_stripper']['cluster_id']
+  'client-auth'           => node['repose']['client_auth'          ]['cluster_id'],
+  'content-type-stripper' => node['repose']['content_type_stripper']['cluster_id'],
+  'derp'                  => node['repose']['derp'                 ]['cluster_id'],
+  'header-translation'    => node['repose']['header_translation'   ]['cluster_id'],
+  'header-normalization'  => node['repose']['header_normalization' ]['cluster_id'],
+  'ip-identity'           => node['repose']['ip_identity'          ]['cluster_id'],
+  'rate-limiting'         => node['repose']['rate_limiting'        ]['cluster_id'],
+  'slf4j-http-logging'    => node['repose']['slf4j_http_logging'   ]['cluster_id']
 }
 
 template "#{node['repose']['config_directory']}/system-model.cfg.xml" do
