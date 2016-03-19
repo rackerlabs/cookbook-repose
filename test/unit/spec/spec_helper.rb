9ca68f4ec8f685390721063eeede7cf55afa745a
Scott Nelson Windels <scott.windels@rackspace.com>
2016-03-18 17:01:09 -0700

    Update install case setup, add spec_helper and default_spec tests

---
+++ test/unit/spec/spec_helper.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+require 'rspec/expectations'
+require 'chefspec'
+require 'chefspec/berkshelf'
+require 'chef/application'
+
+def stub_resources
+end
+
+at_exit { ChefSpec::Coverage.report! }
