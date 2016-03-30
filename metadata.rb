d3a90d428ac78d27911d173e35616a1f79e5d8e9
Nick Silkey <nick.silkey@rackspace.com>
2016-03-30 08:44:05 -0500

    Fix foodcritic complaints

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license 'All rights reserved'
 description 'Installs/Configures repose'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '3.2.0'
+issues_url 'https://github.com/rackerlabs/cookbook-repose/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/rackerlabs/cookbook-repose' if respond_to?(:source_url)
 
 depends 'apt'
 depends 'yum', '~> 3.0'
