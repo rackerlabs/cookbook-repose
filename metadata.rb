44830a370959686c6e6299211d615ddaa79cbb0b
Michael Burns <michael@mirwin.net>
2017-03-27 02:02:13 -0700

    change email maintainer to cookbook-repose@
    per https://github.com/rackerlabs/cookbook-repose/issues/56#issuecomment-210053414

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'repose'
 maintainer 'Rackspace Hosting'
-maintainer_email 'cit-ops@rackspace.com'
+maintainer_email 'cookbook-repose@rackspace.com'
 license 'All rights reserved'
 description 'Installs/Configures repose'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
