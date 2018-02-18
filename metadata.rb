7b2f4d8a764daccf5fc1ada82c354244f31f9014
Michael Burns <michael@mirwin.net>
2018-02-17 23:17:20 -0800

    update metadata with chef_version

--- metadata.rb
+++ metadata.rb
@@ -7,9 +7,13 @@ license 'All rights reserved'
 description 'Installs/Configures repose'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '3.4.0'
+chef_version '>= 12' if respond_to?(:chef_version)
 issues_url 'https://github.com/rackerlabs/cookbook-repose/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/rackerlabs/cookbook-repose' if respond_to?(:source_url)
 
 depends 'apt'
 depends 'yum'
 depends 'yum-epel'
+
+supports 'centos'
+supports 'ubuntu'
